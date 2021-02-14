419a60c5a53181a23a3443ddedf6fa1126525344
Martin <martinpellizzer@gmail.com>
2021-02-14 15:16:08 +0100

    starting program

--- stm32/main.c
+++ stm32/main.c
@@ -1,3 +1,9 @@
+const char c1 = 100;
+const char c2 = 200;
+const char c3 = 255;
+
+char test_val = 1;
+
 int main() 
 {
 	while(1) 
--- stm32/stm32_startup.c
+++ stm32/stm32_startup.c
@@ -1,10 +1,19 @@
 #include<stdint.h>
 
 #define SRAM_START 	0x20000000U
-#define SRAM_SIZE 	(128 * 1024)
+#define SRAM_SIZE 	(128U * 1024U)
 #define SRAM_END 	((SRAM_START) + (SRAM_SIZE))
 
 #define STACK_START	SRAM_END
+
+extern uint32_t _etext;
+extern uint32_t _sdata;
+extern uint32_t _edata;
+
+extern uint32_t _sbss;
+extern uint32_t _ebss;
+
+int main(void);
 
 void Reset_Handler(void);
 void NMI_Handler(void)			__attribute__((weak, alias("Default_Handler")));
@@ -204,6 +213,25 @@ void Default_Handler(void)
 
 void Reset_Handler(void)
 {
+	uint32_t size = (uint32_t)&_edata -(uint32_t)&_sdata;
 
+
+	uint8_t *pDst = (uint8_t*)&_sdata;
+	uint8_t *pSrc = (uint8_t*)&_etext;
+
+	for(uint32_t i = 0; i < size; i++)
+	{
+		*pDst++ = *pSrc++;
+	}
+
+	size = (uint32_t)&_ebss -(uint32_t)&_sbss;
+	pDst = (uint8_t*)&_sbss;
+
+	for(uint32_t i = 0; i < size; i++)
+	{
+		*pDst++ = 0; 
+	}
+
+	main();
 }
 
